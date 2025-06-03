9777257022fcaa5d05357302cd75c9313c377819
ksb0896 <186450057+ksb0896@users.noreply.github.com>
2025-06-03 19:58:21 +0530

    added swagger-api-config

--- src/main/java/com/ksb/micro/order/config/OpenAPIConfig.java
+++ src/main/java/com/ksb/micro/order/config/OpenAPIConfig.java
@@ -1,2 +1,20 @@
-package com.ksb.micro.order.config;public class OpenAPIConfig {
+package com.ksb.micro.order.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI orderServiceAPI(){ //documentation to describe your api service
+        return new OpenAPI()
+                .info(new Info().title("Order Service API")
+                        .description("This is the REST API for Inventory Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")));
+    }
 }
